c6036c3149bf8ea45efaad7dae269a79616c1216
Edison Montes M. <edison.montes@gmail.com>
2025-08-22 12:39:57 -0400

    Create const.py

---
+++ custom_components/media_downloader/const.py
@@ -0,0 +1,17 @@
+DOMAIN = "media_downloader"
+
+CONF_DOWNLOAD_DIR = "download_dir"
+CONF_OVERWRITE = "overwrite"
+
+DEFAULT_OVERWRITE = False
+
+SERVICE_DOWNLOAD_FILE = "download_file"
+
+ATTR_URL = "url"
+ATTR_SUBDIR = "subdir"
+ATTR_FILENAME = "filename"
+ATTR_OVERWRITE = "overwrite"
+ATTR_TIMEOUT = "timeout"
+
+EVENT_DOWNLOAD_STARTED = "media_downloader_download_started"
+EVENT_DOWNLOAD_COMPLETED = "media_downloader_download_completed"
